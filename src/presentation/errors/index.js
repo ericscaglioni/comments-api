67bb69c116010661e429b00cd91fe6eadab2f74f
Eric Scaglioni <ericsf34@gmail.com>
2021-03-10 09:55:20 -0300

    refactor: remove unecessary code

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,9 +1,7 @@
 const { MissingParamError } = require('./missing-param-error')
 const { ServerError } = require('./server-error')
-const { InvalidParamError } = require('./invalid-param-error')
 
 module.exports = {
     MissingParamError,
-    ServerError,
-    InvalidParamError
+    ServerError
 }
